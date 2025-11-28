c6e5a6e4df0d12f7344e46d08eb0d02a0608ef6a
Matt Borland <matt@mattborland.com>
2025-11-28 09:17:22 +0100

    Add properties testing

---
+++ test/test_integer_type_properties.cpp
@@ -0,0 +1,51 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/safe_numbers.hpp>
+#include <type_traits>
+
+using namespace boost::safe_numbers;
+
+template <typename T>
+void test_properties()
+{
+    using basis = detail::underlying_type_t<T>;
+
+    static_assert(std::is_trivially_copyable_v<T>);
+    static_assert(std::is_trivially_destructible_v<T>);
+
+    static_assert(std::is_standard_layout_v<T>);
+    static_assert(std::has_unique_object_representations_v<T>);
+
+    static_assert(sizeof(T) == sizeof(basis));
+    static_assert(alignof(T) == alignof(basis));
+
+    static_assert(std::is_default_constructible_v<T>);
+    static_assert(std::is_trivially_default_constructible_v<T>);
+    static_assert(std::is_nothrow_default_constructible_v<T>);
+
+    static_assert(std::is_copy_constructible_v<T>);
+    static_assert(std::is_trivially_copy_constructible_v<T>);
+    static_assert(std::is_nothrow_copy_constructible_v<T>);
+
+    static_assert(std::is_copy_assignable_v<T>);
+    static_assert(std::is_trivially_copy_assignable_v<T>);
+    static_assert(std::is_nothrow_copy_assignable_v<T>);
+
+    static_assert(std::is_move_constructible_v<T>);
+    static_assert(std::is_trivially_move_constructible_v<T>);
+
+    static_assert(std::is_move_assignable_v<T>);
+    static_assert(std::is_trivially_move_assignable_v<T>);
+}
+
+int main()
+{
+    test_properties<u8>();
+    test_properties<u16>();
+    test_properties<u32>();
+    test_properties<u64>();
+
+    return 0;
+}
