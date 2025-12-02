7ffd9234e8e8761060a4c5f1e877de2454f8a1d0
Matt Borland <matt@mattborland.com>
2025-12-02 16:42:08 +0100

    Add test for construction from bool

---
+++ test/test_unsigned_construction_from_bool.cpp
@@ -0,0 +1,25 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/safe_numbers.hpp>
+#include <boost/core/lightweight_test.hpp>
+
+template <typename T>
+void test()
+{
+    const T value {true};
+    BOOST_TEST_EQ(value, T{1});
+}
+
+int main()
+{
+    using namespace boost::safe_numbers;
+
+    test<u8>();
+    test<u16>();
+    test<u32>();
+    test<u64>();
+
+    return boost::report_errors();
+}
