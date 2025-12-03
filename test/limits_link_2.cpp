4dcc3323f8f45f7c71bd6bca6f9fcaf1c3e8a3a2
Matt Borland <matt@mattborland.com>
2025-12-03 09:14:08 +0100

    Fix copy-paste error

--- test/limits_link_2.cpp
+++ test/limits_link_2.cpp
@@ -3,7 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/decimal/charconv.hpp>
+#include <boost/safe_numbers.hpp>
 #include <limits>
 
 void test_odr_use( int const* );
@@ -16,11 +16,10 @@ template<typename T> void test()
 
 void f2()
 {
-    test<boost::decimal::decimal32_t>();
-    test<boost::decimal::decimal64_t>();
-    test<boost::decimal::decimal128_t>();
+    using namespace boost::safe_numbers;
 
-    test<boost::decimal::decimal_fast32_t>();
-    test<boost::decimal::decimal_fast64_t>();
-    test<boost::decimal::decimal_fast128_t>();
+    test<u8>();
+    test<u16>();
+    test<u32>();
+    test<u64>();
 }
