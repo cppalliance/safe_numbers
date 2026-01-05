fa7f65077b9346e4176dd10523376fa5fa422112
Matt Borland <matt@mattborland.com>
2026-01-05 16:49:47 -0500

    Add benchmarks for multiplication

--- test/benchmarks/benchmark_unsigned_operations.cpp
+++ test/benchmarks/benchmark_unsigned_operations.cpp
@@ -61,15 +61,14 @@ inline constexpr std::size_t N {10'000'000};
 inline std::mt19937_64 rng(42);
 
 template <typename T>
-BOOST_NOINLINE auto generate_vector()
+auto generate_vector()
 {
     using value_type = std::conditional_t<detail::is_library_type_v<T>, detail::underlying_type_t<T>, T>;
 
     std::vector<T> values;
     values.reserve(N);
 
-    boost::random::uniform_int_distribution<value_type> dist {std::numeric_limits<value_type>::min(),
-                                                              std::numeric_limits<value_type>::max() / 2U - 1U};
+    boost::random::uniform_int_distribution<value_type> dist {0, static_cast<value_type>(std::sqrt(std::numeric_limits<value_type>::max()) - 1U)};
 
     for (std::size_t i {}; i < N; ++i)
     {
@@ -79,6 +78,20 @@ BOOST_NOINLINE auto generate_vector()
     std::sort(values.begin(), values.end(), std::greater<>());
 
     return values;
+}
+
+template <typename T, typename U>
+auto generate_vector(const std::vector<U>& values)
+{
+    std::vector<T> result;
+    result.reserve(values.size());
+
+    for (const auto& value : values)
+    {
+        result.emplace_back(static_cast<T>(value));
+    }
+
+    return result;
 }
 
 template <typename T, typename Func>
@@ -131,6 +144,12 @@ auto benchmark_subtraction(const std::vector<T>& values, const char* type)
     return benchmark_op(values, std::minus<>(), type, "sub");
 }
 
+template <typename T>
+auto benchmark_multiplication(const std::vector<T>& values, const char* type)
+{
+    return benchmark_op(values, std::multiplies<>(), type, "mul");
+}
+
 #ifdef _MSC_VER
 #pragma optimize("", on)
 #endif
@@ -150,7 +169,7 @@ int main()
     {
         std::cout << "8-bit Unsigned Integers\n";
         const auto builtin_values{generate_vector<std::uint8_t>()};
-        const auto lib_values{generate_vector<u8>()};
+        const auto lib_values{generate_vector<u8>(builtin_values)};
 
         auto builtin_runtime = benchmark_addition(builtin_values, "std::uint8_t");
         auto lib_runtime = benchmark_addition(lib_values, "boost::sn::u8");
@@ -159,11 +178,15 @@ int main()
         builtin_runtime = benchmark_subtraction(builtin_values, "std::uint8_t");
         lib_runtime = benchmark_subtraction(lib_values, "boost::sn::u8");
         print_runtime_ratio(lib_runtime, builtin_runtime);
+
+        builtin_runtime = benchmark_multiplication(builtin_values, "std::uint8_t");
+        lib_runtime = benchmark_multiplication(lib_values, "boost::sn::u8");
+        print_runtime_ratio(lib_runtime, builtin_runtime);
     }
     {
         std::cout << "\n16-bit Unsigned Integers\n";
         const auto builtin_values{generate_vector<std::uint16_t>()};
-        const auto lib_values{generate_vector<u16>()};
+        const auto lib_values{generate_vector<u16>(builtin_values)};
 
         auto builtin_runtime = benchmark_addition(builtin_values, "std::uint16_t");
         auto lib_runtime = benchmark_addition(lib_values, "boost::sn::u16");
@@ -172,11 +195,15 @@ int main()
         builtin_runtime = benchmark_subtraction(builtin_values, "std::uint16_t");
         lib_runtime = benchmark_subtraction(lib_values, "boost::sn::u16");
         print_runtime_ratio(lib_runtime, builtin_runtime);
+
+        builtin_runtime = benchmark_multiplication(builtin_values, "std::uint16_t");
+        lib_runtime = benchmark_multiplication(lib_values, "boost::sn::u16");
+        print_runtime_ratio(lib_runtime, builtin_runtime);
     }
     {
         std::cout << "\n32-bit Unsigned Integers\n";
         const auto builtin_values{generate_vector<std::uint32_t>()};
-        const auto lib_values{generate_vector<u32>()};
+        const auto lib_values{generate_vector<u32>(builtin_values)};
 
         auto builtin_runtime = benchmark_addition(builtin_values, "std::uint32_t");
         auto lib_runtime = benchmark_addition(lib_values, "boost::sn::u32");
@@ -185,11 +212,15 @@ int main()
         builtin_runtime = benchmark_subtraction(builtin_values, "std::uint32_t");
         lib_runtime = benchmark_subtraction(lib_values, "boost::sn::u32");
         print_runtime_ratio(lib_runtime, builtin_runtime);
+
+        builtin_runtime = benchmark_multiplication(builtin_values, "std::uint32_t");
+        lib_runtime = benchmark_multiplication(lib_values, "boost::sn::u32");
+        print_runtime_ratio(lib_runtime, builtin_runtime);
     }
     {
         std::cout << "\n64-bit Unsigned Integers\n";
         const auto builtin_values{generate_vector<std::uint64_t>()};
-        const auto lib_values{generate_vector<u64>()};
+        const auto lib_values{generate_vector<u64>(builtin_values)};
 
         auto builtin_runtime = benchmark_addition(builtin_values, "std::uint64_t");
         auto lib_runtime = benchmark_addition(lib_values, "boost::sn::u64");
@@ -197,6 +228,10 @@ int main()
 
         builtin_runtime = benchmark_subtraction(builtin_values, "std::uint64_t");
         lib_runtime = benchmark_subtraction(lib_values, "boost::sn::u64");
+        print_runtime_ratio(lib_runtime, builtin_runtime);
+
+        builtin_runtime = benchmark_multiplication(builtin_values, "std::uint64_t");
+        lib_runtime = benchmark_multiplication(lib_values, "boost::sn::u64");
         print_runtime_ratio(lib_runtime, builtin_runtime);
     }
 
