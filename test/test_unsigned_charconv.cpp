03bcd180d8ac02471d56c359b55c55df00ebb77f
Matt Borland <matt@mattborland.com>
2026-01-07 11:07:36 -0500

    Add testing and compile testing

---
+++ test/test_unsigned_charconv.cpp
@@ -0,0 +1,122 @@
+// Copyright 2026 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/core/lightweight_test.hpp>
+
+#if defined(__clang__)
+#  pragma clang diagnostic push
+#  pragma clang diagnostic ignored "-Wold-style-cast"
+#  pragma clang diagnostic ignored "-Wundef"
+#  pragma clang diagnostic ignored "-Wconversion"
+#  pragma clang diagnostic ignored "-Wsign-conversion"
+#  pragma clang diagnostic ignored "-Wfloat-equal"
+#  pragma clang diagnostic ignored "-Wsign-compare"
+#  pragma clang diagnostic ignored "-Woverflow"
+
+#  if (__clang_major__ >= 10 && !defined(__APPLE__)) || __clang_major__ >= 13
+#    pragma clang diagnostic ignored "-Wdeprecated-copy"
+#  endif
+
+#elif defined(__GNUC__)
+#  pragma GCC diagnostic push
+#  pragma GCC diagnostic ignored "-Wold-style-cast"
+#  pragma GCC diagnostic ignored "-Wundef"
+#  pragma GCC diagnostic ignored "-Wconversion"
+#  pragma GCC diagnostic ignored "-Wsign-conversion"
+#  pragma GCC diagnostic ignored "-Wsign-compare"
+#  pragma GCC diagnostic ignored "-Wfloat-equal"
+#  pragma GCC diagnostic ignored "-Woverflow"
+
+#elif defined(_MSC_VER)
+#  pragma warning(push)
+#  pragma warning(disable : 4389)
+#  pragma warning(disable : 4127)
+#  pragma warning(disable : 4305)
+#  pragma warning(disable : 4309)
+#endif
+
+#include <boost/random/uniform_int_distribution.hpp>
+
+#ifdef __clang__
+#  pragma clang diagnostic pop
+#elif defined(__GNUC__)
+#  pragma GCC diagnostic pop
+#elif defined(_MSC_VER)
+#  pragma warning(pop)
+#endif
+
+// Ignore [[nodiscard]] on the test that we know are going to throw
+#ifdef __clang__
+#  pragma clang diagnostic push
+#  pragma clang diagnostic ignored "-Wunused-result"
+#elif defined(__GNUC__)
+#  pragma GCC diagnostic push
+#  pragma GCC diagnostic ignored "-Wunused-result"
+#elif defined(_MSC_VER)
+#  pragma warning (push)
+#  pragma warning (disable: 4834)
+#endif
+
+#ifdef BOOST_SAFE_NUMBERS_BUILD_MODULE
+
+import boost.safe_numbers;
+
+#else
+
+#include <boost/safe_numbers.hpp>
+#include <random>
+#include <cstring>
+#include <limits>
+#include <type_traits>
+#include <iostream>
+
+#endif
+
+using namespace boost::safe_numbers;
+
+static std::mt19937_64 rng {42};
+
+template <typename T>
+void test(T value, int base = 10)
+{
+    char buffer[256];
+    const auto r_to {to_chars(buffer, buffer + sizeof(buffer), value, base)};
+    BOOST_TEST(r_to.ec == std::errc{});
+
+    T return_val {};
+    const auto r_from {from_chars(buffer, r_to.ptr, return_val, base)};
+    BOOST_TEST(r_from.ec == std::errc{});
+
+    BOOST_TEST_EQ(value, return_val);
+}
+
+template <typename T>
+void test_roundtrip()
+{
+    using basis_type = detail::underlying_type_t<T>;
+    boost::random::uniform_int_distribution<basis_type> dist {std::numeric_limits<basis_type>::min(),
+                                                              std::numeric_limits<basis_type>::max()};
+
+    for (int base = 2; base <= 36; ++base)
+    {
+        // Guarantee that we get the max and min cases
+        test(std::numeric_limits<T>::max(), base);
+        test(std::numeric_limits<T>::min(), base);
+
+        for (int i {}; i < 1024; ++i)
+        {
+            test(T{dist(rng)}, base);
+        }
+    }
+}
+
+int main()
+{
+    test_roundtrip<u8>();
+    test_roundtrip<u16>();
+    test_roundtrip<u32>();
+    test_roundtrip<u64>();
+
+    return boost::report_errors();
+}
