f268df0494eced238d31fd73c9be8749d390656e
Matt Borland <matt@mattborland.com>
2026-01-08 10:04:59 -0500

    Port in int128

---
+++ include/boost/safe_numbers/detail/int128/detail/constants.hpp
@@ -0,0 +1,24 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_SAFE_NUMBERS_DETAIL_INT128_DETAIL_CONSTANTS_HPP
+#define BOOST_SAFE_NUMBERS_DETAIL_INT128_DETAIL_CONSTANTS_HPP
+
+#include <cstdint>
+#include <limits>
+
+namespace boost {
+namespace int128 {
+namespace detail {
+
+BOOST_SAFE_NUMBERS_INLINE_CONSTEXPR_VARIABLE std::uint64_t low_word_mask {(std::numeric_limits<std::uint64_t>::max)()};
+
+template <typename T>
+BOOST_SAFE_NUMBERS_INLINE_CONSTEXPR_VARIABLE T offset_value_v = static_cast<T>((std::numeric_limits<std::uint64_t>::max)());
+
+} // namespace detail
+} // namespace int128
+} // namespace boost
+
+#endif // BOOST_SAFE_NUMBERS_DETAIL_INT128_DETAIL_CONSTANTS_HPP
