5dffed4658faf17f9365848f86bf6a065e78e24d
Matt Borland <matt@mattborland.com>
2026-01-30 15:22:48 -0500

    Use boost charconv instead of std::charconv

--- include/boost/safe_numbers/charconv.hpp
+++ include/boost/safe_numbers/charconv.hpp
@@ -6,40 +6,34 @@
 #define BOOST_SAFE_NUMBERS_CHARCONV_HPP
 
 #include <boost/safe_numbers/detail/unsigned_integer_basis.hpp>
+#include <boost/safe_numbers/detail/concepts.hpp>
 
 #ifndef BOOST_SAFE_NUMBERS_BUILD_MODULE
 
+#include <boost/charconv.hpp>
 #include <charconv>
 #include <concepts>
 
 #endif // BOOST_SAFE_NUMBERS_BUILD_MODULE
 
-#if defined(__cpp_lib_constexpr_charconv) && __cpp_lib_constexpr_charconv >= 202207L
-#  define BOOST_SAFE_NUMBERS_CONSTEXPR_CHARCONV constexpr
-#else
-#  define BOOST_SAFE_NUMBERS_CONSTEXPR_CHARCONV
-#endif
-
 namespace boost::safe_numbers {
 
-template <std::unsigned_integral BasisType>
-BOOST_SAFE_NUMBERS_CONSTEXPR_CHARCONV
-auto from_chars(const char* first, const char* last, detail::unsigned_integer_basis<BasisType>& value, int base = 10)
-    -> std::from_chars_result
+template <detail::unsigned_integral BasisType>
+constexpr auto from_chars(const char* first, const char* last, detail::unsigned_integer_basis<BasisType>& value, int base = 10)
+    -> charconv::from_chars_result
 {
     BasisType result {};
-    const auto r {std::from_chars(first, last, result, base)};
+    const auto r {charconv::from_chars(first, last, result, base)};
     value = detail::unsigned_integer_basis<BasisType>{result};
 
     return r;
 }
 
-template <std::unsigned_integral BasisType>
-BOOST_SAFE_NUMBERS_CONSTEXPR_CHARCONV
-auto to_chars(char* first, char* last, const detail::unsigned_integer_basis<BasisType> value, int base = 10)
-    -> std::to_chars_result
+template <detail::unsigned_integral BasisType>
+constexpr auto to_chars(char* first, char* last, const detail::unsigned_integer_basis<BasisType> value, int base = 10)
+    -> charconv::to_chars_result
 {
-    return std::to_chars(first, last, static_cast<BasisType>(value), base);
+    return charconv::to_chars(first, last, static_cast<BasisType>(value), base);
 }
 
 } // namespace boost::safe_numbers
