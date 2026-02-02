887d02901461d44d53fdcb64800215d44b555370
Matt Borland <matt@mattborland.com>
2026-02-02 15:01:15 -0500

    Define overflow policy enum

---
+++ include/boost/safe_numbers/detail/overflow_policy.hpp
@@ -0,0 +1,18 @@
+// Copyright 2026 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_SAFE_NUMBERS_DETAIL_OVERFLOW_POLICY_HPP
+#define BOOST_SAFE_NUMBERS_DETAIL_OVERFLOW_POLICY_HPP
+
+namespace boost::safe_numbers::detail {
+
+enum class overflow_policy
+{
+    throw_exception,
+    saturate
+};
+
+} // namespace boost::safe_numbers::detail
+
+#endif // BOOST_SAFE_NUMBERS_DETAIL_OVERFLOW_POLICY_HPP
