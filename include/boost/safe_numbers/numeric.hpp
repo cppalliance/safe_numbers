1f6d268c0acde31e16b62ba128cee8b92c9eefee
Matt Borland <matt@mattborland.com>
2026-02-19 09:52:09 -0500

    Add consteval gcd overload

--- include/boost/safe_numbers/numeric.hpp
+++ include/boost/safe_numbers/numeric.hpp
@@ -5,6 +5,7 @@
 #ifndef BOOST_SAFE_NUMBERS_NUMERIC_HPP
 #define BOOST_SAFE_NUMBERS_NUMERIC_HPP
 
+#include <boost/safe_numbers/detail/verified_type_basis.hpp>
 #include <boost/safe_numbers/detail/type_traits.hpp>
 #include <boost/safe_numbers/detail/int128/numeric.hpp>
 
@@ -29,6 +30,22 @@ constexpr auto gcd(const T m, const T n) noexcept -> T
     else
     {
         return T{static_cast<underlying_type>(std::gcd(static_cast<underlying_type>(m), static_cast<underlying_type>(n)))};
+    }
+}
+
+template <detail::non_bounded_integral_library_type T>
+consteval auto gcd(const detail::verified_type_basis<T> m, const detail::verified_type_basis<T> n) noexcept -> detail::verified_type_basis<T>
+{
+    using underlying_type = detail::underlying_type_t<T>;
+    using return_type = detail::verified_type_basis<T>;
+
+    if constexpr (std::is_same_v<underlying_type, int128::uint128_t> || std::is_same_v<underlying_type, int128::int128_t>)
+    {
+        return return_type{T{int128::gcd(static_cast<underlying_type>(m), static_cast<underlying_type>(n))}};
+    }
+    else
+    {
+        return return_type{T{static_cast<underlying_type>(std::gcd(static_cast<underlying_type>(m), static_cast<underlying_type>(n)))}};
     }
 }
 
