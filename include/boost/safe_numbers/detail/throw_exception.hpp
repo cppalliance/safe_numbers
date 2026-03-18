8f577a3b510339538b01ccf25e85df37098df5cf
Matt Borland <matt@mattborland.com>
2026-03-18 09:45:47 -0500

    Add placeholder version of lib throw exception

---
+++ include/boost/safe_numbers/detail/throw_exception.hpp
@@ -0,0 +1,28 @@
+// Copyright 2026 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+//
+// BOOST_THROW_EXCEPTION fits our needs for CPU tasks
+// We need to define our own version of thrown exception so that we can also handle the device environment
+// This is the only file in the lib that should attempt inclusion of boost/throw_exception.hpp
+
+#ifndef BOOST_SAFE_NUMBERS_THROW_EXCEPTION_HPP
+#define BOOST_SAFE_NUMBERS_THROW_EXCEPTION_HPP
+
+#ifndef BOOST_SAFE_NUMBERS_BUILD_MODULE
+
+#include <boost/throw_exception.hpp>
+
+#endif // BOOST_SAFE_NUMBERS_BUILD_MODULE
+
+// This is defined only in the device compilation step
+// as opposed to __NVCC__
+#ifndef __CUDA_ARCH__
+
+#define BOOST_SAFE_NUMBERS_THROW_EXCEPTION(err_ptr, x) static_cast<void>(err_ptr); BOOST_THROW_EXCEPTION(x)
+
+#else
+
+#endif // __CUDA_ARCH__
+
+#endif // BOOST_SAFE_NUMBERS_THROW_EXCEPTION_HPP
