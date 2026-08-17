a8c0bb62f12037a15eccc3bc9bd4f8024c0108f7
Matt Borland <matt@mattborland.com>
2026-08-17 14:06:37 -0400

    Add policy options for users

--- include/boost/safe_numbers/overflow_policy.hpp
+++ include/boost/safe_numbers/overflow_policy.hpp
@@ -19,6 +19,28 @@ BOOST_SAFE_NUMBERS_EXPORT enum class overflow_policy
     widen,
 };
 
+// The kind of error being reported to a user defined handler.
+// divide_by_zero covers modulo by zero as well; the message distinguishes them.
+// invalid_operation and nan_operation are produced by the floating point types only.
+BOOST_SAFE_NUMBERS_EXPORT enum class error_kind
+{
+    overflow,
+    underflow,
+    divide_by_zero,
+    invalid_operation,
+    nan_operation,
+};
+
+// Tag types selecting the built-in policies through the basic_* alias templates,
+// e.g. basic_u8<saturating>. A user defined handler type is used the same way:
+// basic_u8<my_handler>.
+
+BOOST_SAFE_NUMBERS_EXPORT struct throwing {};
+
+BOOST_SAFE_NUMBERS_EXPORT struct saturating {};
+
+BOOST_SAFE_NUMBERS_EXPORT struct strict {};
+
 } // namespace boost::safe_numbers
 
 #endif // BOOST_SAFE_NUMBERS_OVERFLOW_POLICY_HPP
